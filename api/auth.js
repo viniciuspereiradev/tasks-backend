7ad16e2cfe0015f347ad425c9480e8af9557e004
Vinicius Pereira <viniciuspg0071@gmail.com>
2021-05-16 19:12:27 -0300

    correção auth tratamento do email do usuario

--- api/auth.js
+++ api/auth.js
@@ -10,7 +10,7 @@ module.exports = app => {
         }
 
         const user = await app.db("users")
-            .where({ email: req.body.email })
+            .whereRaw("LOWER(email) = LOWER(?)", req.body.email)
             .first()
 
         if (user) {
--- api/user.js
+++ api/user.js
@@ -15,7 +15,10 @@ module.exports = app => {
 
 
             app.db("users")
-                .insert({ name: req.body.name, email: req.body.email, password })
+                .insert({ 
+                    name: req.body.name, 
+                    email: req.body.email.toLowerCase(), 
+                    password })
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(400).json(err))
         })
