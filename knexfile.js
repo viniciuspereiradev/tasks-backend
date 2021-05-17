d21f9ba3d0f0200d59ef2e38378642cf3c36c756
Vinicius Pereira <viniciuspg0071@gmail.com>
2021-05-17 14:29:39 -0300

    config banco de dados

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,12 @@
 module.exports = {
 
 
-  client: 'postgres://xgpjvrusfpowfm:fed01ca765c0246feecd31e3af63b037f597f8e4d9804416929477a3f19026f9@ec2-3-234-22-132.compute-1.amazonaws.com:5432/d8tmtok9n98ud0',
+  client: 'postgresql',
   connection: {
-    database: 'd8tmtok9n98ud0',
-    user: 'xgpjvrusfpowfm',
-    password: 'fed01ca765c0246feecd31e3af63b037f597f8e4d9804416929477a3f19026f9'
+    host : 'ec2-3-234-22-132.compute-1.amazonaws.com',
+    user : 'xgpjvrusfpowfm',
+    password : 'fed01ca765c0246feecd31e3af63b037f597f8e4d9804416929477a3f19026f9',
+    database : 'd8tmtok9n98ud0'
   },
   pool: {
     min: 2,
